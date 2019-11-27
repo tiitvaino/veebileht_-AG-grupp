c25242b79336d24aceb9d2b77cee8cbb63fbcf99
tiitvaino <56859547+tiitvaino@users.noreply.github.com>
2019-11-27 11:31:49 +0200

    scripti ja template põhi
    scripti ja template põhi

---
+++ menu.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+<!-- See script on pärit lehelt https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav_push -->
